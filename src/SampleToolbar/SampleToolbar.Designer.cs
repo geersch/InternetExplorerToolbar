a2d2970088612ef312698ad2c8086c99cff24d5b
Christophe Geers <geersch@gmail.com>
2014-05-31 16:50:13 +0200

    Added source code

---
+++ src/SampleToolbar/SampleToolbar.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace SampleToolbar
+{
+    partial class SampleToolbar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SampleToolbar));
+            this.toolStrip = new System.Windows.Forms.ToolStrip();
+            this.btnHello = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip
+            // 
+            this.toolStrip.BackColor = System.Drawing.Color.Transparent;
+            this.toolStrip.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStrip.GripMargin = new System.Windows.Forms.Padding(0);
+            this.toolStrip.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnHello});
+            this.toolStrip.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip.Name = "toolStrip";
+            this.toolStrip.Padding = new System.Windows.Forms.Padding(0);
+            this.toolStrip.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.toolStrip.Size = new System.Drawing.Size(150, 24);
+            this.toolStrip.Stretch = true;
+            this.toolStrip.TabIndex = 1;
+            this.toolStrip.Text = "toolStrip";
+            // 
+            // btnHello
+            // 
+            this.btnHello.Image = ((System.Drawing.Image)(resources.GetObject("btnHello.Image")));
+            this.btnHello.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnHello.Name = "btnHello";
+            this.btnHello.Size = new System.Drawing.Size(73, 21);
+            this.btnHello.Text = "Click me";
+            this.btnHello.Click += new System.EventHandler(this.btnHello_Click);
+            // 
+            // SampleToolbar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.toolStrip);
+            this.MaxSize = new System.Drawing.Size(150, 24);
+            this.MinSize = new System.Drawing.Size(150, 24);
+            this.Name = "SampleToolbar";
+            this.Size = new System.Drawing.Size(150, 24);
+            this.Title = "";
+            this.ViewMode = ((BandObjectLib.DBIMF)((BandObjectLib.DBIMF.USECHEVRON | BandObjectLib.DBIMF.TOPALIGN)));
+            this.toolStrip.ResumeLayout(false);
+            this.toolStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip;
+        private System.Windows.Forms.ToolStripButton btnHello;
+
+    }
+}
---
+++ src/SampleToolbar/SampleToolbar.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using BandObjectLib;
+
+namespace SampleToolbar
+{        
+    [Guid("88272A6E-9989-4c15-8536-D4F7C4E2D891")]
+    [Description("Sample Toolbar")]
+    [BandObject("Sample Toolbar", BandObjectStyle.ExplorerToolbar | 
+        BandObjectStyle.Horizontal, HelpText = "A sample toolbar with a single button")]      
+    public partial class SampleToolbar : BandObject
+    {
+        public SampleToolbar()
+        {
+            InitializeComponent();            
+
+            this.toolStrip.RenderMode = ToolStripRenderMode.ManagerRenderMode;
+            this.toolStrip.Renderer = new NoBorderToolStripRenderer();
+        }
+
+        private void btnHello_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello world!");
+        }
+    }
+
+    ///
+    /// ToolStripSystemRenderer that overrides OnRenderToolStripBorder to avoid painting the borders.
+    ///
+    internal class NoBorderToolStripRenderer : ToolStripSystemRenderer
+    {
+        protected override void OnRenderToolStripBorder(ToolStripRenderEventArgs e) { }
+    } 
+}
